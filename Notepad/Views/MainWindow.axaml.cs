fb91ae18eb88247cf74d160f227ed038c4b1c265
Алина Махаева <alina.makhaeva@yandex.ru>
2023-03-12 23:33:05 +0700

    Добавьте файлы проекта.

---
+++ Notepad/Views/MainWindow.axaml.cs
@@ -0,0 +1,26 @@
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Layout;
+using Avalonia.Media;
+using Notepad.ViewModels;
+
+namespace Notepad.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new MainWindowViewModel();
+        }
+
+        private void DBLtoupe(object sender, RoutedEventArgs e)
+        {
+            var mwvm = (MainWindowViewModel?)DataContext;
+            var src = e.Source;
+            if (src == null || mwvm == null) return;
+            var name = src.GetType().Name;
+            if (name == "Image" || name == "ContentPresenter" || name == "TextBlock") mwvm.DBLtoupe();
+        }
+    }
+}
